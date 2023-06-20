c3894b9023f41435240315393a50fbfdcf95e090
Sagar Singh Negi <sagarssn23@gmail.com>
2023-06-20 13:03:00 +0530

    Assignment 2.1-todoServer --> Handle invalid route

--- 02-nodejs/todoServer.js
+++ 02-nodejs/todoServer.js
@@ -106,6 +106,9 @@ app.delete('/todos/:id', (req, res) => {
   } else res.status(404).send("No todo found");
 });
 
+app.use((req, res, next) => {
+  res.status(404).send("Route not found")
+})
 
 app.listen(port, () => {
   console.log(`Todo Server is listening on port ${port}`);
